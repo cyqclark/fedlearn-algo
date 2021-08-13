b4292748e043be58e45d2d6a7fd0938b44b73b30
jiazhou wang <jiazhou.wang@gmail.com>
2021-08-13 01:23:03 -0700

    remove import

--- docs/api/source/conf.py
+++ docs/api/source/conf.py
@@ -18,7 +18,6 @@ sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../..'))
 sys.path.insert(0, os.path.abspath('../../../..'))
 
-import core.client.client
 
 # -- Project information -----------------------------------------------------
 
