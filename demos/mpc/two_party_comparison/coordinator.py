ce6c9e470cc2c412de57821992416a4180107a5a
jiazhou wang <jiazhou.wang@gmail.com>
2021-08-24 15:33:24 -0700

    add disclaimer

--- demos/mpc/two_party_comparison/coordinator.py
+++ demos/mpc/two_party_comparison/coordinator.py
@@ -1,3 +1,16 @@
+# Copyright 2021 Fedlearn authors.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Production wrapper code for 1 out of n OT
 """
@@ -90,24 +103,24 @@ class ActiveWrapper(Bob, Server):
         self.result = self.parse_result()
         return self.result
 
-    # training part
-    def init_training_control(self):
+    def init_inference_control(self):
         return None
 
     def get_next_phase(self, phase):
         return None
 
-    def is_training_continue(self):
-        return None
-
     def is_inference_continue(self):
         return None
 
-    def post_training_session(self):
-        return None
-    
-    def init_inference_control(self):
+    def post_inference_session(self):
         return None
 
-    def post_inference_session(self):
+    # training part
+    def init_training_control(self):
+        return None
+
+    def is_training_continue(self):
+        return None
+    
+    def post_training_session(self):
         return None
