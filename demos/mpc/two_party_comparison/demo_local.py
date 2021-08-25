5ff076a38beb3fb83def80d6d7e9f24463ad5658
jiazhou wang <jiazhou.wang@gmail.com>
2021-08-25 12:11:57 -0700

    add local code

--- demos/mpc/two_party_comparison/demo_local.py
+++ demos/mpc/two_party_comparison/demo_local.py
@@ -25,19 +25,75 @@ import client
 import coordinator
 import EncodedNumber
 
-import unittest   # The test framework
+sys.path.append(os.getcwd())
+from core.entity.common.machineinfo import MachineInfo
 
 def test_comparison():
-
-    active = coordinator.ActiveWrapper(1.)
+    active = coordinator.ActiveWrapper(1., None, None)
     passive = client.PassiveWrapper(1.)
 
     active_number = numpy.random.rand()
     passive_number = numpy.random.rand()
     active.set_raw_number(active_number)
     passive.set_raw_number(passive_number)
-        
+
     message_1st = active.init_request()
+
+    message_2nd = passive.init_response(message_1st)
+
+    message_3th = active.second_request(message_2nd)
+
+    message_4th = passive.second_response(message_3th)
+
+    result = active.parse_final(message_4th)
+
+    print("Raw number:")
+    print("Active: %.6f"%active_number)
+    print("Passve: %.6f"%passive_number)
+    print("Secure comparison result: Passive > Active ? %s"%str(result))
+
+
+def test_comparison_grpc():
+
+    active_client_info = MachineInfo("127.0.0.1", "8001", "0")
+    passive_client_info = MachineInfo("127.0.0.1", "8002", "0")
+
+    active = coordinator.ActiveWrapper(1.,
+                                       active_client_info=active_client_info,
+                                       passive_client_info=[passive_client_info])
+    passive = client.PassiveWrapper(1.
+                                    )
+    client_map = {passive_client_info: passive,
+                }
+    active_number = numpy.random.rand()
+    passive_number = numpy.random.rand()
+    active.set_raw_number(active_number)
+    passive.set_raw_number(passive_number)
+
+    phase = "0"
+
+    init_requests = active.create_init_request()
+
+    responses = {}
+    for client_info, reqi in init_requests.items():
+        c = client_map[client_info]
+        responses[client_info] = c.control_flow_client(reqi.phase_id, reqi)
+
+    while True:
+        phase = active.get_next_phase(phase)
+        print("Phase %s start..."%phase)
+        requests = active.control_flow_coordinator(phase, responses)
+        responses = {}
+        if active.is_inference_continue():
+            for client_info, reqi in requests.items():
+                c = client_map[client_info]
+                responses[client_info] = c.control_flow_client(reqi.phase_id, reqi.copy())
+        else:
+            break
+
+    import pdb
+    pdb.set_trace()
+    message_1st = active.create_init_request()
 
     message_2nd = passive.init_response(message_1st)
 
