95b8bff5421bd9f5d8394183de365192c1eb3533
jiazhou wang <jiazhou.wang@gmail.com>
2021-08-25 17:25:35 -0700

    update demo code

--- demos/mpc/two_party_comparison/demo_local.py
+++ demos/mpc/two_party_comparison/demo_local.py
@@ -30,7 +30,7 @@ from core.entity.common.machineinfo import MachineInfo
 
 def test_comparison():
     active = coordinator.ActiveWrapper(1., None, None)
-    passive = client.PassiveWrapper(1.)
+    passive = client.PassiveWrapper(1., None)
 
     active_number = numpy.random.rand()
     passive_number = numpy.random.rand()
@@ -61,8 +61,8 @@ def test_comparison_grpc():
     active = coordinator.ActiveWrapper(1.,
                                        active_client_info=active_client_info,
                                        passive_client_info=[passive_client_info])
-    passive = client.PassiveWrapper(1.
-                                    )
+    passive = client.PassiveWrapper(1.,
+                                    client_info = passive_client_info)
     client_map = {passive_client_info: passive,
                 }
     active_number = numpy.random.rand()
@@ -91,17 +91,7 @@ def test_comparison_grpc():
         else:
             break
 
-    import pdb
-    pdb.set_trace()
-    message_1st = active.create_init_request()
-
-    message_2nd = passive.init_response(message_1st)
-
-    message_3th = active.second_request(message_2nd)
-
-    message_4th = passive.second_response(message_3th)
-
-    result = active.parse_final(message_4th)
+    result = active.result
 
     print("Raw number:")
     print("Active: %.6f"%active_number)
@@ -112,4 +102,7 @@ def test_comparison_grpc():
 
 
 if __name__ == "__main__":
+    print("Local demo")
     test_comparison()
+    print("Remote demo")
+    test_comparison_grpc()
